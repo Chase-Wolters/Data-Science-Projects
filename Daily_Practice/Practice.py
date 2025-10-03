cbb09cb416bb87b5e933e666018dbdd721aa7447
Chase-Wolters <cwolters05@gmail.com>
2025-10-03 15:16:21 -0500

    created search functionality

--- Daily_Practice/Practice.py
+++ Daily_Practice/Practice.py
@@ -3,10 +3,9 @@
 # then ask a user who they want to search for 
 
 # insert directory info
-directory = []
-
-# empty string to hold insert directory options
-dir_info = ''
+directory = {"chase wolters": "123-432-2444", 
+             "evelyn wilson": "321-325-8740",
+             "shawn wolters": "298-294-8597"}
 
 insert_info = 0
 while insert_info == 0:
@@ -14,10 +13,17 @@ while insert_info == 0:
 see full directory
 Search Person
 Insert new info
-If done with directory, please enter 0: ''')
-    if choice == 'Search Person':
-         dir_info = input('Hello! please enter info:')
-         directory =+ dir_info
-    if choice == '0':
+If done with directory, please enter 0: ''').lower()
+    if choice == 'see full directory':
+        print(directory.keys())
+    elif choice == 'search person':
+        person_info = input("Please type the name of the person who's info you want to know: ").lower()
+        if person_info in directory.keys():
+            print(directory[person_info])
+    elif choice == 'insert new info':
+        dir_info = input('Please enter info:')
+        directory.append(dir_info)
+    elif choice == '0':
         insert_info = 1
+
     
