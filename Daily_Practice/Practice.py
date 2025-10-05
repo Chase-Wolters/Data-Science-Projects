aa9bb24326d3b91f5752b886ad23f7b0a0fe1122
Chase-Wolters <cwolters05@gmail.com>
2025-10-05 15:30:23 -0500

    commit

--- Daily_Practice/Practice.py
+++ Daily_Practice/Practice.py
@@ -4,4 +4,15 @@
 # the computer will translate it. 
 # however, upon typing this I realized if a cop can get a picture of the liscence plate, then they dont need the alphanumeric code 
 
-alphanumberic = { 'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta'}
+alphanumberic = { 'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta',
+                 'e': 'echo', 'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett', 'k': 'kilo', 'l': 'lima',
+                 'm': 'mike', 'n': 'november', 'o': 'oscar', 'p': 'papa', 'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
+                   'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'xray', 'y': 'yankee', 'z': 'zulu'}
+
+word = input('Please enter word and I will translate to alphanumeric code: ')
+
+# for i in word:
+#     if i == alphanumberic.keys()
+#     print(alphanumberic[i])
+
+print(alphanumberic.keys())
