ee7d16acd508b29914f3048cf1cf30bbad20ebd6
Chase-Wolters <cwolters05@gmail.com>
2025-10-05 18:41:20 -0500

    values of names are able to be ranked

--- Daily_Practice/mini_projects/Name_value.py
+++ Daily_Practice/mini_projects/Name_value.py
@@ -2,6 +2,7 @@
 # goal: create a reusable function, when given a name--using the letters position in the american alphabet,
 # calulate the value of the name
 # potentially include a functionality to keep track and rank the names
+import numpy as np
 
 # dictionary for associating letters with number values
 alphabet = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 
@@ -13,10 +14,13 @@ count = 0
 # initialize counter
 name_total = 0
 
+# name rank system
+name_hist = {}
 question = input('Would you like to enter a name? ').lower()
 while count == 0:
     if question == 'yes':
             name = input('Please type a name: ').lower()
+            name_hist[name] = 0
             # loop through letters in name
             for i in name:
                 if i in alphabet.keys():
@@ -27,6 +31,7 @@ while count == 0:
                     print("please use only letters")
             print(f'The total for your name is: {name_total}')
             name2 = input('Would you like to enter another name? ').lower()
+            name_hist[name] = name_total
             if name2 == 'yes':
                 name_total = 0
                 continue
@@ -36,5 +41,5 @@ while count == 0:
     else:
         print('Have a nice day')
         count = 1
-    
-
+rankings = np.sort(list(name_hist.values()))[::-1]
+print(rankings)
