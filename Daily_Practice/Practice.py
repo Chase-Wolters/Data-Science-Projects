947341c943ed2d562b7f392a08f77420bbfe865c
Chase-Wolters <cwolters05@gmail.com>
2025-10-05 12:02:51 -0500

    came up with idea and basic skills needed to accomplish task

---
+++ Daily_Practice/Practice.py
@@ -0,0 +1,7 @@
+# new py file to practice script writing
+# this script will use dictionaries to convert letters in a word into there alphanumeric word
+# potential use cases: police can scan a license plate and instead of needing to read it out and potentially have an error,
+# the computer will translate it. 
+# however, upon typing this I realized if a cop can get a picture of the liscence plate, then they dont need the alphanumeric code 
+
+alphanumberic = { 'a': 'alpha', 'b': 'bravo', 'c': 'charlie', 'd': 'delta'}
